ce0ac119131bc2cd15a56bbb73a6c350cd234c7a
yillmad <90701683+yillmad@users.noreply.github.com>
2023-09-28 08:30:58 -0500

    google maps

--- scripts/hambutton.js
+++ scripts/hambutton.js
@@ -1,7 +1,14 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const mapContainer = document.querySelector('.map-container')
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+
+	if (navigation.classList.contains('open')) {
+		mapContainer.style.display ='none';
+	} else {
+		mapContainer.style.display ='block';
+	}
 });
