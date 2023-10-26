315dfadfbb06766dbd1a4a014aeb7f2a5498dc7d
yillmad <90701683+yillmad@users.noreply.github.com>
2023-10-26 13:59:57 -0500

    visits

---
+++ scripts/weather.js
@@ -0,0 +1,26 @@
+const apiKey = "164e55133784eda8dd008adca3141746";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Arequipa&units=metric";
+const weatherStatus = document.querySelector(".weather-status");
+
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".degree").innerHTML = Math.round(data.main.temp) + " °C";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherStatus.innerHTML = "Cloudy";
+  } else if (data.weather[0].main == "Clear") {
+    weatherStatus.innerHTML = "Clear";
+  } else if (data.weather[0].main == "Rain") {
+    weatherStatus.innerHTML = "Rainy";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherStatus.innerHTML = "Drizzly";
+  } else if (data.weather[0].main == "Mist") {
+    weatherStatus.innerHTML = "Misty";
+  }
+}
+
+checkWeather();
