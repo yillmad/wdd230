cb7816e8bf8ae852c93a908c52eb8c5d8fe52598
yillmad <90701683+yillmad@users.noreply.github.com>
2023-11-10 19:33:40 -0500

    activities

--- scripts/link.js
+++ scripts/link.js
@@ -1,27 +1,25 @@
 const baseURL = "https://yillmad.github.io/wdd230/";
-const linksURL = `${baseURL}data/links.json`;
-const ActivitiesElement = document.querySelector("#activities");
+const linksURL = "https://yillmad.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    keys= Object.keys(data);
-    displayLinks(data,keys);
-}
-
-function displayLinks(data,keys) {
-    keys.forEach(key => {
-        const li = document.createElement("li");
-        li.textContent = key + ": ";
-        data[key].forEach(link => {
-            const a = document.createElement("a");
-            a.setAttribute("href", link.link);
-            a.setAttribute("target", "_blank");
-            a.textContent = link.name + " | ";
-            li.appendChild(a);
-        });
-        ActivitiesElement.appendChild(li);
+    displayLinks(data);
+  }
+  
+  function displayLinks(lessons) {
+    const linksList = document.getElementById("links");
+  
+    lessons.lessons.forEach(lesson => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
+        Lesson ${lesson.lesson}:
+        ${lesson.links.map(link => `
+          <a href="${link.url}" target="_blank">${link.title}</a>
+        `).join(" | ")}
+      `;
+      linksList.appendChild(listItem);
     });
-}
-
-getLinks();
+  }
+  
+  getLinks();
