002490c766a9efdcec3b0df03d024048a8307546
yillmad <90701683+yillmad@users.noreply.github.com>
2023-11-14 17:06:56 -0500

    almost

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,24 +1,42 @@
+const companiesDiv = document.querySelector('#companies');
+const gridbutton = document.querySelector('#gridBtn');
+const listbutton = document.querySelector('#listBtn');
+const display = document.querySelector('#companies');
+
 fetch('data/members.json')
   .then(response => response.json())
   .then(data => {
     const companies = data.companies;
 
-    const companiesDiv = document.getElementById('companies');
-
     companies.forEach(company => {
       const companyDiv = document.createElement('div');
-      companyDiv.classList.add('company-item'); 
+      companyDiv.classList.add('company-card'); 
 
       companyDiv.innerHTML = `
-        <h2>${company.name}</h2>
-        <p>${company.address}</p>
-        <p>${company.phoneNumbers}</p>
-        <a href="${company.webURL}" target="_blank">${company.webURL}</a>
-        <img src="images/${company.img}" alt="${company.name} Image">
-        <!-- Add more elements to display other information -->
+        <div class="company-logo">
+            <img src="images/${company.img}" alt="${company.name} Image">
+        </div>
+        <div class="company-info">
+            <h2>${company.name}</h2>
+            <p>${company.address}</p>
+            <p>${company.phoneNumbers}</p>
+            <a href="${company.webURL}" target="_blank">${company.webURL}</a>
+            <p class="other">${company.other}</p>
+        </div>
       `;
 
       companiesDiv.appendChild(companyDiv);
     });
   })
   .catch(error => console.log(error));
+
+
+  gridbutton.addEventListener("click",() => {
+    display.classList.add("directory-grid");
+    display.classList.remove("directory-list");
+  });
+
+  listbutton.addEventListener("click",() => {
+    display.classList.add("directory-list");
+    display.classList.remove("directory-grid");
+  });
